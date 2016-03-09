02d69bbab993a0c6879782345b02a047daf09480
UncleSandwich <UncleSandwich@users.noreply.github.com>
2016-03-09 23:59:14 +0800

    add font.py

---
+++ font.py
@@ -0,0 +1,24 @@
+#This file contains all the alphabet we need to display
+
+A="\
+    *       \n\
+   * *      \n\
+  *   *     \n\
+ *     *    \n\
+*********   \n\
+*       *   \n\
+*       *   \n\
+*       *   \n\
+*       *   \n\
+"
+B="\
+********    \n\
+*       *   \n\
+*        *  \n\
+*       *   \n\
+*********   \n\
+*       *   \n\
+*        *  \n\
+*       *   \n\
+********    \n\
+"
